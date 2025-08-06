e81740b48e107e457b15f7b8ff905c3b20e6c9d5
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-06 03:01:28 +0100

    Add login UI and custom button implementation
    - Created a new colors.dart file to define color constants for the app.
    - Implemented a LoginUi widget with a welcome message, an image, and a custom button.
    - Developed a MyButton widget that utilizes the defined color constants and provides a styled elevated button.

---
+++ lib/my_ui/login_ui.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_app/my_ui/my_button/my_button.dart';
+
+class LoginUi extends StatelessWidget {
+  const LoginUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Welcome to MyVideoApp',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          Image.asset("assets/images/MeetingImage.png"),
+          MyButton(txt: 'Join Now or Meeting'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/my_ui/my_button/my_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_app/my_tools/colors.dart';
+
+class MyButton extends StatelessWidget {
+  final String txt;
+  const MyButton({super.key, required this.txt});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text(txt, style: TextStyle(fontSize: 16, color: Colors.black)),
+      style: ElevatedButton.styleFrom(
+        maximumSize: Size(double.infinity, 60),
+        backgroundColor: MyButtonColor,
+      ),
+    );
+  }
+}
