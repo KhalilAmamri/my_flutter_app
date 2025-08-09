c40a07cb868a310388e67cebf7da0bde1d7f622c
KhalilAmamri <KhalilAmamri@outlook.com>
2025-08-09 20:39:21 +0100

    Refactor JoinMeetUI to use StatefulWidget and implement meeting ID and user name input fields

--- lib/my_ui/join_meet.dart
+++ lib/my_ui/join_meet.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter_app/my_code/auth_code.dart';
 
-class JoinMeetUI extends StatelessWidget {
-  const JoinMeetUI({super.key});
+class JoinMeetUI extends StatefulWidget {
+  @override
+  State<JoinMeetUI> createState() => _JoinMeetUIState();
+}
+
+class _JoinMeetUIState extends State<JoinMeetUI> {
+  //
+  late TextEditingController meetingID;
+  late TextEditingController userName;
+  final AuthCode _authCode = AuthCode();
+  @override
+  void initState() {
+    meetingID = TextEditingController(text: 'This  is a meeting ID');
+    userName = TextEditingController(
+      text: _authCode.currentUser?.displayName ?? '',
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Join Meeting')),
-      body: Center(child: Text('Join Meeting UI will be implemented here')),
+      body: Column(
+        children: [
+          TextField(
+            controller: meetingID,
+            keyboardType: TextInputType.number,
+            maxLines: 1,
+            decoration: InputDecoration(
+              fillColor: Colors.blueGrey,
+              filled: true,
+              border: InputBorder.none,
+              hintText: 'Enter Meeting ID',
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: userName,
+            keyboardType: TextInputType.number,
+            maxLines: 1,
+            decoration: InputDecoration(
+              fillColor: Colors.blueGrey,
+              filled: true,
+              border: InputBorder.none,
+              hintText: 'User Name',
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
